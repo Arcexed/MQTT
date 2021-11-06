d9a58f53ab2f7efcd2ed9a135986130bf9aaf01b
Andrey Lyn <arcex.off@gmail.com>
2021-11-07 01:53:02 +0200

    Some changes to the unversioned files

---
+++ MQTT.Data/Migrations/20211021082706_AddingModelUserAndDeviceInModels.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MQTT.Data.Migrations
+{
+    public partial class AddingModelUserAndDeviceInModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventsDevices_Devices_DeviceId",
+                table: "EventsDevices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventsUsers_Users_UserId",
+                table: "EventsUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Measurements_Devices_DeviceId",
+                table: "Measurements");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeviceId",
+                table: "Measurements",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "EventsUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeviceId",
+                table: "EventsDevices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicIp",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrivateIp",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MqttToken",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventsDevices_Devices_DeviceId",
+                table: "EventsDevices",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventsUsers_Users_UserId",
+                table: "EventsUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Measurements_Devices_DeviceId",
+                table: "Measurements",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventsDevices_Devices_DeviceId",
+                table: "EventsDevices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_EventsUsers_Users_UserId",
+                table: "EventsUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Measurements_Devices_DeviceId",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "MqttToken",
+                table: "Devices");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeviceId",
+                table: "Measurements",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "EventsUsers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "DeviceId",
+                table: "EventsDevices",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PublicIp",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrivateIp",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventsDevices_Devices_DeviceId",
+                table: "EventsDevices",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EventsUsers_Users_UserId",
+                table: "EventsUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Measurements_Devices_DeviceId",
+                table: "Measurements",
+                column: "DeviceId",
+                principalTable: "Devices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ MQTT.Data/Migrations/20211021085916_ChangesInDeviceModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MQTT.Data.Migrations
+{
+    public partial class ChangesInDeviceModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Users_UserId",
+                table: "Devices");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Devices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Users_UserId",
+                table: "Devices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Users_UserId",
+                table: "Devices");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Devices",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Users_UserId",
+                table: "Devices",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
